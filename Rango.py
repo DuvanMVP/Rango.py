4a7a22c7634accc86d9bd679255a55899ac7ac12
DuvanMVP <158522532+DuvanMVP@users.noreply.github.com>
2024-03-01 11:45:59 -0500

    Add files via upload

---
+++ Rango.py
@@ -0,0 +1,10 @@
+CantidadInt = int(input('¿Cantidad de numeros?'))
+contadorInt = 1
+contadornumerosInt = 0
+while CantidadInt <= CantidadInt:
+    numeroInt = int(input('Ingrese un numero'))
+    numeroInt = int(input('Ingrese un numero'))
+    if numeroInt>= 10 and numeroInt <=20:
+        contadornumerosInt += 1
+        contadorInt += 1
+    print('La cantidad de numeros en ek rango de 10 a 20 son: ', contadornumerosInt)
